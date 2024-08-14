81dfc9905a62e5b51348ada7adbd7b3434d1f858
Suhaila14 <suhailaafathima@gmail.com>
2024-08-14 08:34:06 +0530

    Update Connectdb.js

--- server/Connectdb.js
+++ server/Connectdb.js
@@ -2,7 +2,7 @@ const mongoose = require ("mongoose");
 const Connectdb = async () => {
     try{
         mongoose.set('strictQuery', false); // This will remove some unwanted warnings.
-        const conn = await mongoose.connect(process.env.Mongodb_Url);
+        const conn = await mongoose.connect(process.env.MongoDb_Url);
         console.log(`Database Connected ${conn.connection.host}`);
     } catch(error){
         console.log(error);
